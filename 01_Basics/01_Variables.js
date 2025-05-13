5a8df4e78f8fb671d8e619604e13f14c305c29a5
Tanvi1829 <mapuskartanvi6@gmail.com>
2025-05-13 06:44:05 +0000

    Variables and datatypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,23 @@
+const accountId = 1233
+let accountEmail = "tanvimapuskar18@gmail.com"
+var accountPassword = "1829"
+accountCity = "Mumbai"
+let accountState;
+
+
+/* Important Notes: 
+        - not use var because of issue in block of scope & functional scope  
+        - if variable decleare but not assign any value in variable then print the undefined 
+*/
+
+
+
+accountEmail = "tanvi18@gmail.com"
+accountPassword = "182902"
+accountCity = "Jaipur"
+
+
+console.table([accountId, accountEmail,  accountPassword, accountCity, accountState]);
+
+
+
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js as a newer version 
+
+// alert(3+3) // we are use nodejs not a browser 
+
+
+let name = "Tanvi" //string datatype
+let age = 22 //number datatype
+let isLoggedIn = true //boolean datatype
+
+
+/*
+    Important Notes: 
+        - code readability should be high
+        - number => 2 to power 52 -> range 
+        - bigint -> for large number 
+        - string => "" 
+        - boolean => true/false
+        - null => standalone value
+        - undefined => not assigned value 
+        - symbol => unique
+
+    * Object: 
+        - undefined => return undefined
+        - null => return object 
+    
+*/
+

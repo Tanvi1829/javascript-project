44b858d5aa2ffec0577602d76156c328708466a0
Tanvi1829 <mapuskartanvi6@gmail.com>
2025-05-13 07:09:17 +0000

    Conversion of Datatypes

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,34 @@
+let score = "Tanvi"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = "Tanvi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 29
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+/* 
+    Important Notes: 
+        - "33" => 33
+        - "33abc" => NaN (Not a Number)
+        - true => 1 
+        - false => 0
+        - 1 => true
+        - 0 => false
+        - "" => false
+        - "Name or any values" => true 
+*/
+
+
+
+
+

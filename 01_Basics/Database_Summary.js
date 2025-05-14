e1acb19956ed6ab9c3e30e5576b896175729aa98
Tanvi1829 <mapuskartanvi6@gmail.com>
2025-05-14 07:50:16 +0000

    add link

--- 01_Basics/Database_Summary.js
+++ 01_Basics/Database_Summary.js
@@ -20,6 +20,9 @@
             - objects
             - functions
 
+        - Link for study 
+            - https://262.ecma-international.org/5.1/#sec-11.4.3
+
 */
 
 // Primitive datatypes(Call by Value)
